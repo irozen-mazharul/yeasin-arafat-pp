73414eb45cd75a8cf1050dbb25dda01094573379
Mohammad Eliyas Miah <mazharul.cs.eng@gmail.com>
2023-08-08 12:34:53 +0600

    'enviornment-setup-done'

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\View\View;
 
 class HomeController extends Controller
 {
---
+++ resources/views/frontend/home/home.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Yeasin Arafat LLC</title>
+{{--    Favicon--}}
+    <link rel="icon" href="{{asset('asset/img/favicon/favicon.ico')}}" type="image/icon">
+{{--    Bootstrap- 5--}}
+    <link rel="stylesheet" href="{{asset('asset/css/bootstrap.css')}}">
+{{--    Style CSS--}}
+    <link rel="stylesheet" href="{{asset('asset/css/style.css')}}">
+</head>
+<body>
+
+<script src=""></script>
+<script src="{{asset('asset/js/code.jquery.com_jquery-3.7.0.js')}}"></script>
+<script src="{{asset('asset/js/bootstrap.js')}}"></script>
+<script src="{{asset('asset/js/main.js')}}"></script>
+</body>
+</html>
